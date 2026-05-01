ecd7099c86e715ed13ead104fac77d46de37a46d
ryzenek7 <ryzenreps@gmail.com>
2026-05-01 19:10:21 +0200

    Update stakeacc.js

--- stakeacc.js
+++ stakeacc.js
@@ -6,7 +6,7 @@ const {
 } = require("discord.js");
 
 module.exports = (client) => {
-  const CHANNEL_ID = "1499568863602540645";
+  const CHANNEL_ID = "1499812157246669001";
 
   async function sendPanel() {
     try {
