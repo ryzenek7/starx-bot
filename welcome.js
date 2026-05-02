3c726c35ad99e5c5eb9d32c2141874d6d054d9ae
ryzenek7 <ryzenreps@gmail.com>
2026-05-03 00:29:33 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -12,11 +12,16 @@ module.exports = (client) => {
       const channel = await client.channels.fetch(WELCOME_CHANNEL_ID);
       if (!channel) return console.log("❌ Nie znaleziono kanału welcome");
 
+      // numer osoby na serwerze
+      const memberCount = member.guild.memberCount;
+
       const embed = new EmbedBuilder()
         .setColor("#57F287")
         .setTitle("👋 Witaj na StarX Exchange!")
         .setDescription(
 `**${member}**, witamy na serwerze! 🔥
+
+🎉 Jesteś **${memberCount}** osobą na serwerze!
 
 📌 **Ważne kanały:**
 
