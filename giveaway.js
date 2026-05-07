063873ef8de47383260be1399733fb422cc74ae1
ryzenek7 <ryzenreps@gmail.com>
2026-05-07 21:38:50 +0200

    Update giveaway.js

--- giveaway.js
+++ giveaway.js
@@ -26,6 +26,8 @@ module.exports = (client) => {
         pin: "<:pin:1501697389050986546>",
         zap: "<:zap:1501697151737139350>",
         lock: "<:lock:1501697222901895258>",
+        time: "<:time:1502030015943151868>",
+        users: "<:users:1500243884733894716>",
         green: "<a:green:1501990166082879538>",
         red: "<a:red:1501989543182864535>"
     };
@@ -53,7 +55,7 @@ module.exports = (client) => {
                 .addStringOption(option =>
                     option
                         .setName("czas")
-                        .setDescription("Np. 1h, 1d, 7d")
+                        .setDescription("Np. 10m, 1h, 1d")
                         .setRequired(true)
                 )
                 .addStringOption(option =>
@@ -73,7 +75,7 @@ module.exports = (client) => {
     });
 
     // =========================================
-    // COMMAND HANDLER
+    // INTERACTION
     // =========================================
     client.on(Events.InteractionCreate, async interaction => {
 
@@ -101,68 +103,8 @@ module.exports = (client) => {
 
             participants.set(giveawayId, new Set());
 
-            const embed = new EmbedBuilder()
-
-                .setColor("#2b2d31")
-
-                .setTitle(`${EMOJI.gift} StarX Exchange » GIVEAWAY`)
-
-                .setDescription(
-                    [
-                        `## ${EMOJI.green} Nagroda`,
-                        "```",
-                        `${nagroda}`,
-                        "```",
-                        "",
-                        `## ${EMOJI.pin} Wymagania`,
-                        `> ${wymagania}`,
-                        "",
-                        `## ${EMOJI.zap} Jak dołączyć?`,
-                        `> Kliknij przycisk poniżej`,
-                        "",
-                        `## ${EMOJI.lock} Informacje`,
-                        `> Czas trwania: ${czas}`,
-                        `> Giveaway jest automatyczny`,
-                        `> Winner zostanie wybrany losowo`
-                    ].join("\n")
-                )
-
-                .setImage(
-                    "https://i.imgur.com/4KfOswz_d.webp?maxwidth=760&fidelity=grand"
-                )
-
-                .setFooter({
-                    text: "StarX Exchange • Giveaway System"
-                })
-
-                .setTimestamp();
-
-            const button = new ButtonBuilder()
-
-                .setCustomId(`join_giveaway_${giveawayId}`)
-
-                .setLabel("Dołącz do giveaway")
-
-                .setEmoji("🎉")
-
-                .setStyle(ButtonStyle.Success);
-
-            const row = new ActionRowBuilder()
-                .addComponents(button);
-
-            await channel.send({
-                embeds: [embed],
-                components: [row]
-            });
-
-            await interaction.reply({
-                content:
-                    `${EMOJI.green} Giveaway został utworzony!`,
-                flags: 64
-            });
-
             // =====================================
-            // TIMER
+            // TIME FORMAT
             // =====================================
             let timeMs = 0;
 
@@ -181,10 +123,107 @@ module.exports = (client) => {
                     parseInt(czas) * 24 * 60 * 60 * 1000;
             }
 
+            const endTimestamp =
+                Math.floor(
+                    (Date.now() + timeMs) / 1000
+                );
+
+            // =====================================
+            // EMBED
+            // =====================================
+            const embed = new EmbedBuilder()
+
+                .setColor("#2b2d31")
+
+                .setTitle(
+                    `${EMOJI.gift} StarX Exchange » GIVEAWAY`
+                )
+
+                .setDescription(
+                    [
+                        `## ${EMOJI.green} Nagroda`,
+                        "```",
+                        `${nagroda}`,
+                        "```",
+                        "",
+
+                        `## ${EMOJI.pin} Wymagania`,
+                        `> ${wymagania}`,
+                        "",
+
+                        `## ${EMOJI.zap} Jak dołączyć?`,
+                        `> Kliknij przycisk poniżej`,
+                        "",
+
+                        `## ${EMOJI.lock} Informacje`,
+                        `> ${EMOJI.time} Koniec: <t:${endTimestamp}:R>`,
+                        `> ${EMOJI.users} Uczestnicy: **0**`,
+                        `> Giveaway jest automatyczny`,
+                        `> Winner zostanie wybrany losowo`
+                    ].join("\n")
+                )
+
+                .setImage(
+                    "https://i.imgur.com/4KfOswz_d.webp?maxwidth=760&fidelity=grand"
+                )
+
+                .setFooter({
+                    text:
+                        "StarX Exchange • Giveaway System"
+                })
+
+                .setTimestamp();
+
+            // =====================================
+            // BUTTON
+            // =====================================
+            const button = new ButtonBuilder()
+
+                .setCustomId(
+                    `join_giveaway_${giveawayId}`
+                )
+
+                .setLabel("Dołącz do giveaway")
+
+                .setEmoji("🎉")
+
+                .setStyle(ButtonStyle.Success);
+
+            const row =
+                new ActionRowBuilder()
+                    .addComponents(button);
+
+            const message = await channel.send({
+                embeds: [embed],
+                components: [row]
+            });
+
+            await interaction.reply({
+                content:
+                    `${EMOJI.green} Giveaway został utworzony!`,
+                flags: 64
+            });
+
+            // =====================================
+            // TIMER
+            // =====================================
             setTimeout(async () => {
 
                 const users =
                     [...participants.get(giveawayId)];
+
+                // disable button
+                const disabledButton =
+                    ButtonBuilder.from(button)
+                        .setDisabled(true);
+
+                const disabledRow =
+                    new ActionRowBuilder()
+                        .addComponents(disabledButton);
+
+                await message.edit({
+                    components: [disabledRow]
+                });
 
                 if (users.length <= 0) {
 
@@ -196,7 +235,8 @@ module.exports = (client) => {
                 const winner =
                     users[
                         Math.floor(
-                            Math.random() * users.length
+                            Math.random() *
+                            users.length
                         )
                     ];
 
@@ -252,7 +292,9 @@ module.exports = (client) => {
                 // ALREADY JOINED
                 // =================================
                 if (
-                    users.has(interaction.user.id)
+                    users.has(
+                        interaction.user.id
+                    )
                 ) {
 
                     return interaction.reply({
@@ -266,6 +308,28 @@ module.exports = (client) => {
                 // ADD USER
                 // =================================
                 users.add(interaction.user.id);
+
+                // =================================
+                // UPDATE EMBED
+                // =================================
+                const message =
+                    interaction.message;
+
+                const oldEmbed =
+                    message.embeds[0];
+
+                const updatedEmbed =
+                    EmbedBuilder.from(oldEmbed)
+                        .setDescription(
+                            oldEmbed.description.replace(
+                                /\*\*\d+\*\*/,
+                                `**${users.size}**`
+                            )
+                        );
+
+                await message.edit({
+                    embeds: [updatedEmbed]
+                });
 
                 return interaction.reply({
                     content:
